85197c7a16ab03e8ed94c5941d3772e2691ab663
nwelter1 <nwelter1@gmail.com>
2020-10-18 15:23:43 -0500

    file structure Amazon scraper

---
+++ scraper.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"}
+def extract_url(url):
+    if url.find('www.amazon.com') != -1:
+        index = url.find('/dp')
+        if index != -1:
+            index2 = index +14
+            url = 'https://www.amazon.com' + url[index:index2]
+        else:
+            index = url.find('/gp')
+            if index != -1:
+                index2 = index + 22
+                url = "https://www.amazon.com" + url[index:index2]
+            else:
+                url = None
+    else:
+        url = None
+    return url
