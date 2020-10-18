d53ae399e55399e6c058c2f9b37f60f9a592c625
nwelter1 <nwelter1@gmail.com>
2020-10-18 16:50:29 -0500

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,10 @@
+import re
 import requests
+import html5lib
 from bs4 import BeautifulSoup
 
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"}
-def extract_url(url):
+def extractUrl(url):
     """
     Method takes in one parameter 'url'
     Checks the validity of the Amazon URL and shortens it into a more manageable string.
@@ -22,3 +24,41 @@ def extract_url(url):
     else:
         url = None
     return url
+
+def convertPrice(price):
+    """
+    Method takes in the product's price as a string with currency symbol and converts
+     it into a float value using simple Regex
+    """
+    converted_price = float(re.sub(r"[^\d.]", "", price))
+    return converted_price
+
+
+def getProductDetails(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"}
+    details = {'name': '', 'price': 0, 'deal': True, 'url': ''}
+    _url = extractUrl(url)
+    if _url == '':
+        details = None
+    else: 
+        page = requests.get(url, headers = headers)
+        soup = BeautifulSoup(page.content, 'html5lib')
+        title = soup.find(id="productTitle")
+        price = soup.find(id="priceblock_dealprice")
+
+        # if item is not on sale, look for alternate ID for price
+        if price is None:
+            price = soup.find(id="priceblock_saleprice")
+            details['deal'] = False
+        print(title, price)
+        # Check for availability and add data into dictionary
+        if title is not None and price is not None:
+            details['name'] = title.get_text().strip()
+            details['price'] = convertPrice(price.get_text())
+            details['url'] = _url
+        else:
+            details = None
+
+    return details
+
+print(getProductDetails('https://www.amazon.com/SideTrak-Freestanding-Anti-Glare-Kickstand-Compatible/dp/B08D1TYR4G/ref=sr_1_4?dchild=1&keywords=sidetrak&qid=1603053905&sr=8-4'))
