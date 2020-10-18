f85cbaf69cd21a92f96db4949ce8961217765837
nwelter1 <nwelter1@gmail.com>
2020-10-18 15:33:22 -0500

    clean up

--- scraper.py
+++ scraper.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"}
 def extract_url(url):
+    """
+    Method takes in one parameter 'url'
+    Checks the validity of the Amazon URL and shortens it into a more manageable string.
+    """
     if url.find('www.amazon.com') != -1:
         index = url.find('/dp')
         if index != -1:
